3d00fb0335960415c6354b13bd06f2548e196672
zeolan <zealan74@gmail.com>
2024-08-12 17:03:05 +0300

    Dependecies added. Github workflows added

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import config from "eslint-config-standard";
+import js from "@eslint/js";
+import globals from "globals";
+import typescriptEslintParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+
+  {
+    ignores: [".config/*"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parser: typescriptEslintParser,
+      //parserOptions: {
+      //  project: ["./tsconfig.json"],
+      //createDefaultProgram: true,
+      //},
+      globals: {
+        ...globals.browser,
+        ...globals.jasmine,
+        require: true,
+        process: true,
+        test: true,
+        //cy: true,
+        //Cypress: true,
+      },
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+];
