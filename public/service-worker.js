364d1aedbf61c8644aad2e69a76ffb81fbc4f6df
zeolan <zealan74@gmail.com>
2025-05-25 20:29:13 +0300

    Sync public folder with cra version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Establish a cache name
 // !!! Need to increment CACHE_VERSION to force cache updates.
-const CACHE_VERSION = 429;
+const CACHE_VERSION = 430;
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 const cacheFiles = ["./index.html", "./manifest.json", "./static/js/"];
