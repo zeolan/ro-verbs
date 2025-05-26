6954c4ba3b935249d626540907cb9823219a04b4
zeolan <zealan74@gmail.com>
2025-05-26 16:16:10 +0300

    Footer test added. Vitest added

---
+++ src/tests/Footer.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import { render, screen } from "@testing-library/react";
+import { renderWithProviders } from "./test-utils.tsx";
+import { beforeAll, describe, expect, it, xit } from "vitest";
+
+import Footer from "../components/Footer.tsx";
+
+describe("Test Footer", () => {
+    const NUMBER_OF_VERBS = 100;
+
+    beforeAll(() => {
+        localStorage.setItem("numberOfVerbs", JSON.stringify(NUMBER_OF_VERBS));
+    });
+    it("test author", () => {
+        const { getByTestId, getByText } = renderWithProviders(<Footer />);
+        const footerAuthorElement = getByTestId("footer-author");
+        expect(footerAuthorElement).toBeVisible();
+        expect(footerAuthorElement.textContent).toEqual("Author: Oleksandr_Z");
+    });
+    it("test app version", () => {
+        const { getByTestId, getByText } = renderWithProviders(<Footer />);
+        const footerVersionElement = getByTestId("footer-version");
+        expect(footerVersionElement).toBeVisible();
+        expect(footerVersionElement.textContent).
+            toEqual(`v${import.meta.env.VITE_APP_VERSION}-${NUMBER_OF_VERBS}`);
+    });
+});
--- vitest.config.js
+++ vitest.config.js
@@ -5,5 +5,6 @@ export default defineConfig({
     // ...
     environment: 'jsdom',
     globals: true,
+    setupFiles: ["./src/setupTests.js"],
   },
 })
