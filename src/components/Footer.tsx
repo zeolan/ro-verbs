26af89994456117d202ee49960046b611d2098ec
zeolan <zealan74@gmail.com>
2025-05-26 20:10:48 +0300

    Tests update in progress

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import { useSelector } from "react-redux";
+
+import {
+  getVerbs,
+} from "../store/reducer.ts";
 
 const Footer: React.FC = () => {
-  const numberOfVerbs = localStorage.getItem("numberOfVerbs");
+  // const numberOfVerbs = localStorage.getItem("numberOfVerbs");
   const APP_VERSION = import.meta.env.VITE_APP_VERSION;
+  const numberOfVerbs = useSelector(getVerbs).length;
+
   return (
     <div className="App-footer" data-testid="footer">
       <div className="App-footer-author" data-testid="footer-author">Author: Oleksandr_Z</div>
--- src/tests/Footer.test.tsx
+++ src/tests/Footer.test.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 // import { render, screen } from "@testing-library/react";
 import { renderWithProviders } from "./test-utils.tsx";
-import { beforeAll, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import Footer from "../components/Footer.tsx";
+import type { RootState } from "../store/store.ts";
+import data from "../data.json";
 
 describe("Test Footer", () => {
-    const NUMBER_OF_VERBS = 100;
-
-    beforeAll(() => {
-        localStorage.setItem("numberOfVerbs", JSON.stringify(NUMBER_OF_VERBS));
-    });
+    const NUMBER_OF_VERBS = data.length;
+    const initialState: Partial<RootState> = {
+        main: {
+            numberOfVerbs: NUMBER_OF_VERBS,
+        },
+    };
+    
     it("test author", () => {
-        const { getByTestId, getByText } = renderWithProviders(<Footer />);
+        const { getByTestId } = renderWithProviders(<Footer />);
         const footerAuthorElement = getByTestId("footer-author");
         expect(footerAuthorElement).toBeVisible();
         expect(footerAuthorElement.textContent).toEqual("Author: Oleksandr_Z");
     });
     it("test app version", () => {
-        const { getByTestId, getByText } = renderWithProviders(<Footer />);
+        const { getByTestId } = renderWithProviders(<Footer />, initialState);
         const footerVersionElement = getByTestId("footer-version");
         expect(footerVersionElement).toBeVisible();
         expect(footerVersionElement.textContent).
