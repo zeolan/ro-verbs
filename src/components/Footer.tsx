9b2488c2644d7056b110fb9913c80397a9900179
zeolan <zealan74@gmail.com>
2025-05-26 16:11:14 +0300

    Update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { Paper } from "@mui/material";
 
 const Footer: React.FC = () => {
   const numberOfVerbs = localStorage.getItem("numberOfVerbs");
   const APP_VERSION = import.meta.env.VITE_APP_VERSION;
   return (
-    <div className="App-footer">
-      <Paper elevation={0} sx={{ display: "flex" }}>
-        <div className="App-footer-author">Author: Oleksandr_Z</div>
-        <div className="App-footer-version">
-          {`v${APP_VERSION}-${numberOfVerbs}`}
-        </div>
-      </Paper>
+    <div className="App-footer" data-testid="footer">
+      <div className="App-footer-author" data-testid="footer-author">Author: Oleksandr_Z</div>
+      <div className="App-footer-version" data-testid="footer-version">
+        {`v${APP_VERSION}-${numberOfVerbs}`}
+      </div>
     </div>
   );
 };
