5a2e3b98b8ce283534a786b6166116732957c854
zeolan <zealan74@gmail.com>
2025-06-03 08:15:22 +0300

    Verbs 437

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Establish a cache name
 // !!! Need to increment CACHE_VERSION to force cache updates.
-const CACHE_VERSION = 437;
+const CACHE_VERSION = 438;
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 const cacheFiles = ["./index.html", "./manifest.json", "./static/js/"];
